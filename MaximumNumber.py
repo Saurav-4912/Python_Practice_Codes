447a9ec0b097dae2583a3b6c6d3d96cb56029dec
Saurav Pawar <sauravpawar46@gmail.com>
2025-01-29 17:42:02 +0530

    Added codes

--- MaximumNumber.py
+++ MaximumNumber.py
@@ -1,9 +1,9 @@
-num = (32,2423,4345,34534,2,43)
+num = (32, 2423, 4345, 34534, 2, 43)
 
 maxNumber = num[0]
 
-for i in range(1,len(num)):
-  if(num[i] > maxNumber):
-    maxNumber = num[i]
+for i in range(1, len(num)):
+    if num[i] > maxNumber:
+        maxNumber = num[i]
 
-print("Maximum Number : " , maxNumber)
+print("Maximum Number : ", maxNumber)
