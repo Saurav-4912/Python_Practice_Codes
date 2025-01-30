78c907d9a68dc337209788e0681ca4f4a9dcc753
Saurav Pawar <sauravpawar46@gmail.com>
2025-01-30 13:57:12 +0530

    Added Basic Code

---
+++ Demo.py
@@ -0,0 +1,94 @@
+"""a = 10
+b=a
+print(id(a))
+print(id(b))
+
+a=b=c=50
+print(a , b , c)"""
+
+
+"""def add():
+  a=10
+  b=20
+  c=a+b
+  print(c)
+
+add()
+"""
+"""
+z = 10
+print(z)
+del z
+print(z)"""
+
+
+# Numbers
+"""
+a = 10
+b=90.54
+c=3j+4
+print ( a  , b, c)
+print(type(a), type(b) , type(c))
+
+tup = (101 , "AAA" , 79.80)
+
+print(type(tup))
+print(tup)
+print("print tuple inside the tuple : ",tup + tup)
+print("Print tuple 3 times : ",tup * 3)
+print(tup[0:2])
+print(tup[0:])
+print(tup[0:1])
+
+import keyword
+
+print(keyword.kwlist)
+
+number = '1000'
+print(type(number))
+
+"""
+
+"""
+a = 45
+b=8
+
+print(a/b)  # 5.625
+print(a//b) #5
+
+print(a+b , a-b , a*b , a%b)
+
+print(a&b)"""
+
+"""numbers = [3, 5, 23, 6, 5, 1, 2, 9, 8]  
+
+for i in numbers:
+  j =i ** 2
+  print(j , end=" ")
+"""
+
+  
+
+"""
+
+print(range(10))
+
+print(list(range(15)))
+
+print(list(range(4,9)))
+
+print(list(range(5, 25, 4)))  
+"""
+
+"""str1 = "saurav"
+str2 = "pawar"
+
+print(str1 + str2) #sauravpawar
+print(str1 *3) #sauravsauravsaurav
+print(str1[2:4]) #ur
+print(str1[:4]) #saur
+print(str2[4]) 
+print(str1[0])
+print('W' in str1)"""
+
+
