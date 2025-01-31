1721679a90a005d8ca0b24f628916b9cce88e398
Saurav Pawar <sauravpawar46@gmail.com>
2025-01-31 12:15:05 +0530

    Changes in factorial code

--- FactorialNumber.py
+++ FactorialNumber.py
@@ -1,7 +1,16 @@
-number = int(input("Enter the number :"))
+"""number = int(input("Enter the number :"))
 
 fact = 1
 for i in range(1, (number + 1)):
     fact *= i
 
-print(fact)
+print(fact)"""
+
+number=10
+
+factorial=1
+
+for i in range(1,number+1):
+  factorial = factorial*i
+
+print(factorial)
