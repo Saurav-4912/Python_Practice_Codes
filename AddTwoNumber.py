3fc5f8bfdfbfd4b92bd470496524cb8e29f8ae94
Saurav Pawar <sauravpawar46@gmail.com>
2025-01-31 12:12:27 +0530

    Add of two number

---
+++ AddTwoNumber.py
@@ -0,0 +1,11 @@
+
+def add(a,b):
+
+  c = a + b
+  return c
+
+number1=int(input('Enter the first number : '))
+number2 = int(input('Enter the second number :'))
+
+result = add(number1 , number2)
+print("Addition of two number : " , result)
